1902b549446ea3b5834a6a446114738df6a0f144
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-16 15:01:48 +0300

    Add strict cross-platform CI

---
+++ include/cgride/project.hpp
@@ -0,0 +1,29 @@
+/**
+ *
+ *  @file project.hpp
+ *  @author Gaspard Kirira
+ *
+ *  Copyright 2026, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/cgride/project
+ *
+ *  Use of this source code is governed by an MIT license
+ *  that can be found in the LICENSE file.
+ *
+ *  Cgride
+ *
+ */
+#ifndef CGRIDE_PROJECT_HPP
+#define CGRIDE_PROJECT_HPP
+
+#include <cgride/project/build_profile.hpp>
+#include <cgride/project/project.hpp>
+#include <cgride/project/requirement.hpp>
+#include <cgride/project/source_set.hpp>
+#include <cgride/project/target.hpp>
+#include <cgride/project/target_kind.hpp>
+#include <cgride/project/validation.hpp>
+#include <cgride/project/version.hpp>
+#include <cgride/project/visibility.hpp>
+
+#endif // CGRIDE_PROJECT_HPP
